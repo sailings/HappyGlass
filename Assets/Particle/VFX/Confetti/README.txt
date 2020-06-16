d8ca8413ef6b85272643da549bec8d6c994b271f
sailings <sailings@126.com>
2020-06-16 22:20:43 +0800

    实现鼠标划线

---
+++ Assets/Particle/VFX/Confetti/README.txt
@@ -0,0 +1,15 @@
+Thanks for purchasing my confetti particle systems collection for Unity 3D!
+
+If you haven’t already, import the package into your Unity 3D project by double clicking the .unitypackage file and then click the 'import' button. Make sure all files are selected, the demo scene can be un-ticked if you don’t want to make use of it.
+
+Using the prefabs:
+
+Simply instantiate a prefab into your scene. Each system is set to loop, so when you no longer need the confetti, destroy the instance.
+
+All particle systems are easily editable, including colour, in the inspector and I’d be happy to help if you’re unsure. Just email me! Enjoy!
+
+For any support request please email hi@lukepeek.com
+
+Thank you!
+
+Luke
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public LineRenderer LineRenderer;
+    private int pointCount = 0;
+    private List<Vector2> pointList = new List<Vector2>();
+    private bool allowDraw = true;
+
+    private void Awake()
+    {
+        LineRenderer.positionCount = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && allowDraw)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (!pointList.Contains(mousePosition))
+            {
+                LineRenderer.positionCount = pointCount + 1;
+                LineRenderer.SetPosition(pointCount, mousePosition);
+                pointCount++;
+                pointList.Add(mousePosition);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            allowDraw = false;
+        }
+    }
+}
