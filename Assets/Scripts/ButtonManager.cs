f0f2dd82704e128907a0fe49d45ee3e5ff723a05
sailings <sailings@126.com>
2020-08-17 22:43:02 +0800

    游戏打包

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Play()
+    {
+        //PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("LevelList");
+    }
+}
